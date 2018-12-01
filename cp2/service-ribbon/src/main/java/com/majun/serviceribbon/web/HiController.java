6d36274dd38fce3d6ffc3395e92094bbc4f89148
Max Ma <max.ma@mtcsys.com>
2018-12-01 10:55:46 +0800

    ribbon+restTemplate

---
+++ cp2/service-ribbon/src/main/java/com/majun/serviceribbon/web/HiController.java
@@ -0,0 +1,20 @@
+package com.majun.serviceribbon.web;
+
+import com.majun.serviceribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+    @Autowired
+    private HelloService helloService;
+
+    @GetMapping(value = "hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+
+}
